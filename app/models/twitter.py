544e2e38255a3ce1c7d05dc7aaccf90f840e103e
alireza <alirezafarjamiwp@gmail.com>
2025-01-03 16:51:49 +0330

    Integrate MongoDB with PyMongo and store Kafka messages in database

--- app/models/twitter.py
+++ app/models/twitter.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class TwitterLoginRequests(BaseModel):
     username: str = Field(..., example="testuser")
-    password: str = Field(..., examples="password123")
+    password: str = Field(..., example="password123")
     phone_number: str = Field(..., example="1234567890")
-    email: EmailStr | None = Field(None, examples="test@example.com")
+    email: EmailStr | None = Field(None, example="test@example.com")
 
